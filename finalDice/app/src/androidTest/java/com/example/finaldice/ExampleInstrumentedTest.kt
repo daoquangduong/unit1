da49081007cad14c4c96d4af1dcbf1da0ae5720e
Sugar Peach <daoquangduong05@gmail.com>
2022-06-29 01:05:59 +0700

    lemonade

--- finalDice/app/src/androidTest/java/com/example/finaldice/ExampleInstrumentedTest.kt
+++ finalDice/app/src/androidTest/java/com/example/finaldice/ExampleInstrumentedTest.kt
@@ -1,24 +1,13 @@
 package com.example.finaldice
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleUnitTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.finaldice", appContext.packageName)
+    fun generates_number(){
+        val dice= MainActivity.Dice(6)
+        val rollResult=dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
